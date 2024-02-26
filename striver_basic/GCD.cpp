9da3b8862232b511df5e61ae5209eb5df9bceea4
Kaushal5033 <kaushalrai8076958922@gmail.com>
2024-02-27 00:15:19 +0530

    new

---
+++ striver_basic/GCD.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int calcGCD(int n, int m){
+    if(n>m){
+        for( int i=0;i<=m;i++)
+            if(n%i||m%i==0){
+                return i;
+            }
+        }
+    }
+    else{
+        for( int i=0;i<=n;i++)
+            if(n%i||m%i==0){
+                return i;
+            }
+        }
+    }
+}
+
+int main() {
+    
+}
--- striver_basic/tempCodeRunnerFile.cpp
+++ striver_basic/tempCodeRunnerFile.cpp
@@ -1,16 +1,1 @@
-else if(a==integer){
-//         cout<<"8 Bytes";
-//     }
-//     else if(a==integer){
-//         cout<<"4 Bytes";
-//     }
-//     else if(a==integer){
-//         cout<<"8 Bytes";
-//     }
-//     else if(a==integer){
-//         cout<<"1 Bytes";
-//     }
-//     else(
-//         cout<<"Enter the correct data type"
-//     )
-// }
+#include<bits/stdc++.h>
